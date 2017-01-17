c5380dbca9712f133ffc729f26c4b9cd254e4720
mtdg-wagner <monsieurwagner@gmail.com>
2017-01-17 10:19:55 -0600

    Single use case added
    Add a case for when you only want to find the cif for one POSCAR in your current folder

--- Symmetry_Finder/symmetry_finder.py
+++ Symmetry_Finder/symmetry_finder.py
@@ -2,11 +2,19 @@ from pymatgen.io.vasp import Poscar
 from pymatgen.io.cif import CifWriter
 import os
 
-with open('files.txt') as f:
-    cwd = os.getcwd()
-    for line in f.readlines():
-        path = cwd + '/' +  line.split()[0]
-        os.chdir(path)
-        p = Poscar.from_file('POSCAR')
-        z = CifWriter(p.structure, symprec=0.001)
-        z.write_file(cwd + '/cifs/' +  '%s_pymat.cif' % line.split()[0])
+if os.path.isfile('files.txt'):
+    with open('files.txt') as f:
+        cwd = os.getcwd()
+        for line in f.readlines():
+            path = cwd + '/' +  line.split()[0]
+            os.chdir(path)
+            p = Poscar.from_file('POSCAR')
+            z = CifWriter(p.structure, symprec=0.001)
+            z.write_file(cwd + '/cifs/' +  '%s_pymat.cif' % line.split()[0])
+        
+else:
+    p = Poscar.from_file('POSCAR')
+    z = CifWriter(p.structure, symprec=0.001)
+    your_name = raw_input('Please provide your cif name without .cif: ')
+    z.write_file(your_name + '.cif')
+
